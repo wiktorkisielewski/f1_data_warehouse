1a58e57803502595f9179a742bea81ced09102d8
wiktorkisielewski <wiktor@kisielewski.dev>
2026-01-11 19:39:11 +0100

    puf

--- f1_dbt/models/marts/dimensions/dim_constructors.sql
+++
@@ -1,6 +0,0 @@
-select
-    constructor_id,
-    constructor_name,
-    nationality,
-    constructor_url
-from {{ ref('stg_constructors') }}
--- f1_dbt/models/marts/dimensions/dim_drivers.sql
+++
@@ -1,7 +0,0 @@
-select
-    driver_id,
-    first_name,
-    last_name,
-    nationality,
-    date_of_birth
-from {{ ref('stg_drivers') }}
