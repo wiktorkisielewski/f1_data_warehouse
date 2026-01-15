e02027b57dab053b5ed5885a406eaa4a2025540c
SebastianSwiczerewski <stigunknown21@gmail.com>
2026-01-15 10:41:16 +0100

    Add wait-for-database logic to ingestion startup

--- ingestion/ingest_all.py
+++ ingestion/ingest_all.py
@@ -1,3 +1,4 @@
+from ingestion.wait_for_db import wait_for_db
 from ingestion.ingest_drivers import main as ingest_drivers
 from ingestion.ingest_constructors import main as ingest_constructors
 from ingestion.ingest_races import main as ingest_races
@@ -5,6 +6,7 @@ from ingestion.ingest_results import main as ingest_results
 
 def main():
     for step in [
+        wait_for_db,
         ingest_drivers,
         ingest_constructors,
         ingest_races,
