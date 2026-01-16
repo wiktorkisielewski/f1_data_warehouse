3e118645640abd978dcc45e03b1f8e6bc828f0a8
SebastianSwiczerewski <stigunknown21@gmail.com>
2026-01-16 18:09:39 +0100

    Add resilient postgres readiness check for containerized ingestion

--- ingestion/wait_for_db.py
+++ ingestion/wait_for_db.py
@@ -1,19 +1,24 @@
 import time
 import psycopg2
 import os
+import logging
 
-def wait_for_db():
+LOG = logging.getLogger("wait_for_db")
+
+def main():
+    LOG.info("Waiting for Postgres...")
+
     while True:
         try:
             psycopg2.connect(
-                host=os.getenv("POSTGRES_HOST"),
-                port=os.getenv("POSTGRES_PORT"),
-                database=os.getenv("POSTGRES_DB"),
+                host=os.getenv("POSTGRES_HOST", "postgres"),
+                port=os.getenv("POSTGRES_PORT", "5432"),
                 user=os.getenv("POSTGRES_USER"),
                 password=os.getenv("POSTGRES_PASSWORD"),
-            ).close()
-            print("Postgres is ready!")
+                dbname=os.getenv("POSTGRES_DB"),
+            )
+            LOG.info("Postgres is ready!")
             break
-        except Exception:
-            print("Waiting for Postgres...")
+        except psycopg2.OperationalError:
+            LOG.info("Postgres not ready yet, retrying...")
             time.sleep(2)
